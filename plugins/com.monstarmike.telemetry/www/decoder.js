77c01b6044ca3d47f44316a169011611d9e94df8
Michael Gardner <mikejr83@gmail.com>
2015-05-16 14:47:45 -0400

    updating the plugin with a lib and changed the plugin.xml to copy the lib to the libs folder.

--- plugins/com.monstarmike.telemetry/www/decoder.js
+++ plugins/com.monstarmike.telemetry/www/decoder.js
@@ -1,35 +1,15 @@
 cordova.define("com.mikejr83.telmetrydecoderplugin", function(require, exports, module) {
-var argscheck = require('cordova/argscheck'),
-    utils = require('cordova/utils'),
-    exec = require('cordova/exec');
+	var argscheck = require('cordova/argscheck'),
+		utils = require('cordova/utils'),
+		exec = require('cordova/exec');
 
 
-var TelmetryDecoder = function() {
-};
+	var TelemetryDecoder = function() {
+	};
 
-//Keyboard.hideKeyboardAccessoryBar = function(hide) {
-//    exec(null, null, "Keyboard", "hideKeyboardAccessoryBar", [hide]);
-//};
-//
-//Keyboard.close = function() {
-//    exec(null, null, "Keyboard", "close", []);
-//};
-//
-//Keyboard.show = function() {
-//    exec(null, null, "Keyboard", "show", []);
-//};
-//
-//Keyboard.disableScroll = function(disable) {
-//    exec(null, null, "Keyboard", "disableScroll", [disable]);
-//};
-//
-///*
-//Keyboard.styleDark = function(dark) {
-// exec(null, null, "Keyboard", "styleDark", [dark]);
-//};
-//*/
-//
-//Keyboard.isVisible = false;
+	TelemetryDecoder.decodeFile = function () {
+		exec("TelemetryDecoder", "decodeFile", []);
+	};
 
-module.exports = TelmetryDecoder;
+	module.exports = TelmetryDecoder;
 });
