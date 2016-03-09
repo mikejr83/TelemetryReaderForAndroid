2ba6f1156f2692e6b890b132c7a8d9562b4adbdc
Michael Gardner <mikejr83@gmail.com>
2016-03-08 21:13:46 -0500

    Changing decoder to use a UDM declaration.

--- plugins/com.monstarmike.telemetry.plugins.tlmDecoder/www/decoder.js
+++ plugins/com.monstarmike.telemetry.plugins.tlmDecoder/www/decoder.js
@@ -1,39 +1,59 @@
-var argscheck = require('cordova/argscheck'),
-  utils = require('cordova/utils'),
-  exec = require('cordova/exec');
+// UDM declaration: https://github.com/umdjs/umd/blob/master/templates/returnExports.js
 
-
-var TelemetryDecoder = function() {
-};
-
-TelemetryDecoder.openFile = function(pass, fail) {
-  exec(function (result) {
-    console.log("Result:", result);
-    if (pass) {
-      pass(result);
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['cordova/argscheck',
+		'cordova/utils',
+		'cordova/exec'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(require('cordova/argscheck'), 
+			require('cordova/utils'),
+			require('cordova/exec'));
+    } else {
+        // Browser globals (root is window)
+        root.returnExports = factory(root.b);
     }
-  }, 
-  function (e) {
-    console.error("Error!", e);
-    if (fail) {
-      fail(e);
-    }
-  }, "TLMDecoder", "openFile", []);
-};
+}(this, function (argscheck, utils, exec) {
 
-TelemetryDecoder.decodeFlight = function(file, flight, pass, fail) {
-  exec(function (result) {
-    console.log("Result:", result);
-    if (pass) {
-      pass(result);
-    }
-  }, 
-  function (e) {
-    console.error("Error!", e);
-    if (fail) {
-      fail(e);
-    }
-  }, "TLMDecoder", "readFlight", [file, flight]);
-};
+	var TelemetryDecoder = function() {
+	};
 
-module.exports = TelemetryDecoder;
+	TelemetryDecoder.openFile = function(pass, fail) {
+	  exec(function (result) {
+		console.log("Result:", result);
+		if (pass) {
+		  pass(result);
+		}
+	  }, 
+	  function (e) {
+		console.error("Error!", e);
+		if (fail) {
+		  fail(e);
+		}
+	  }, "TLMDecoder", "openFile", []);
+	};
+
+	TelemetryDecoder.decodeFlight = function(file, flight, pass, fail) {
+	  exec(function (result) {
+		console.log("Result:", result);
+		if (pass) {
+		  pass(result);
+		}
+	  }, 
+	  function (e) {
+		console.error("Error!", e);
+		if (fail) {
+		  fail(e);
+		}
+	  }, "TLMDecoder", "readFlight", [file, flight]);
+	};
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return TelemetryDecoder;
+}));
